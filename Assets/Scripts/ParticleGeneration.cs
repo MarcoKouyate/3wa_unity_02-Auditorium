1a0febc197158a1caba6e379353dafa779b10f07
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-01-27 13:08:20 +0100

    Particle Generation

---
+++ Assets/Scripts/ParticleGeneration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject _particlePrefab;
+    [SerializeField] private float _interval;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _speed;
+
+    private void Awake()
+    {
+        _nextSpawnTime = Time.time;
+        _particlesFolderTransform = GameObject.Find("Particles").transform;
+    }
+
+    private void Update()
+    {
+        if(Time.time > _nextSpawnTime){
+            SpawnParticle();
+            _nextSpawnTime = Time.time + _interval;
+        }
+    }
+
+    private void SpawnParticle()
+    {
+        //Spawn a particle at a random position around object
+        Vector3 randomPositionInsideCircle = Random.insideUnitCircle * _radius;
+        Vector3 particlePosition = transform.position + randomPositionInsideCircle;
+        GameObject particle = Instantiate(_particlePrefab, particlePosition, transform.rotation, _particlesFolderTransform);
+
+        // Initialize particle movement
+        Rigidbody2D particleRigidbody = particle.GetComponent<Rigidbody2D>();
+        particleRigidbody.AddForce(transform.up * _speed);
+    }
+
+    private float _nextSpawnTime;
+    private Transform _particlesFolderTransform;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+## DESCRIPTION
+The goal is to make a musical puzzle game inspired by Auditorium (https://www.youtube.com/watch?v=Sanx-6zciv4)
+This project is using Unity 2019.4.18 (LTS) version
+
+## HOW TO BUILD
+* Install Git LFS (Large File Storage) if not already installed
+* Clone this repository
+* Locate this project with Unity Hub and open
+
+## TO DO
+(Coming Soon)

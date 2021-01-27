1e13c987eff2b11c0f652885609ae845c3523c31
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-01-27 14:41:09 +0100

    Destroy particles

---
+++ Assets/Scripts/ParticleLifetime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ParticleLifetime : MonoBehaviour
+{
+    [SerializeField] private float _minimalSpeed;
+    [SerializeField] private float _minimalLifetime;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _destroyTime = Time.time + _minimalLifetime;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Time.time > _destroyTime && isSlowEnough())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private bool isSlowEnough()
+    {
+        return _rigidbody.velocity.sqrMagnitude < (_minimalSpeed * _minimalSpeed);
+    }
+
+    private Rigidbody2D _rigidbody;
+    private float _destroyTime;
+}
